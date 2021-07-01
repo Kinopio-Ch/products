48eeed3652b20e2bd358049e973b78ac14a208a9
Kinopio <madeforthenetflix@gmai.com>
2021-07-01 14:22:40 +0800

    add_write_in_txt

--- products.py
+++ products.py
@@ -16,4 +16,9 @@ products[0][0]          #存取products清單
 for p in products:      #用for迴圈存取
     print(p[0], '的價格是', p[1])         #印出product中子清單的第0個位置
 
+with open('product.txt','w') as f:      #開啟product.txt進寫入模式(若電腦有此檔會覆蓋，無此檔會建立)
+    for p in products:      #用for迴圈一個個存取
+        f.write(p[0] + ',' + p[1] + '\n') #將資料寫入檔案，用+號合併字串，\n代表換行       
+
+
 
